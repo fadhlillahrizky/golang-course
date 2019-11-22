343989c33c06837a1f64384afe0c960336554bdb
unknown <rizky.fadhlillah3@gmail.com>
2019-11-22 12:38:45 +0700

    endpoin mysql

---
+++ go-mysql-tutorial/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type User struct {
+	Name string `json:"name"`
+}
+
+func main() {
+	fmt.Println("Go MySQL Tutorial")
+
+	db, err := sql.Open("mysql", "root:dikku123@tcp(127.0.0.1:3307)/testdb")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	//insert
+	// insert, err := db.Query("INSERT INTO users VALUES('ELLIOT')")
+
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+
+	// defer insert.Close()
+
+	// Select
+	results, err := db.Query("SELECT name From users")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for results.Next() {
+		var user User
+
+		err = results.Scan(&user.Name)
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(user.Name)
+	}
+}
--- rest-api-tutorial/main.go
+++ rest-api-tutorial/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
@@ -23,15 +25,23 @@ func allArticles(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(articles)
 }
 
+func testPostArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Test Post endpoint worked")
+
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint Hit")
 
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allArticles)
-	log.Fatal(http.ListenAndServe(":8881", nil))
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	myRouter.HandleFunc("/articles", testPostArticles).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8881", myRouter))
 }
 
 func main() {
