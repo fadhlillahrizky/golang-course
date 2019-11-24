192e474dc1f14e7adbb3393bcb28fdc72bf64f69
unknown <rizky.fadhlillah3@gmail.com>
2019-11-24 15:52:45 +0700

    make handler

--- rest-api-server/server/server.go
+++ rest-api-server/server/server.go
@@ -5,7 +5,8 @@ import (
 	"net/http"
 	"strconv"
 	"time"
-
+	"github.com/gorilla/handlers"
+	"os"
 	RouterFactory "rest-api-server/router"
 
 	
@@ -34,6 +35,16 @@ func NewServer(port int) *Server {
 	server.Addr = ":" + strconv.Itoa(port)
 
 	router := RouterFactory.NewRouter()
+
+	handler := handlers.LoggingHandler(os.Stdout, handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedMethods([]string{"GET","POST","PUT","DELETE","OPTIONS","PATCH"}),
+		handlers.AllowedHeaders([]string{"Content-Type","Origin","Cache-Control","X-App-Token"}),
+		handlers.ExposedHeaders([]string{}),
+		handlers.MaxAge(1000),
+		handlers.AllowCredentials(),
+	)(router.Router))
+	handler = handlers.RecoveryHandler(handlers.PrintRecoveryStack(true))(handler)
 
 	server.HTTPServer = &http.Server{
 		Addr:           server.Addr,
